6296433336142dfa7a6fb65a02b0e14b2fb792b3
LeHiep <huyenvu3101@gmail.com>
2022-12-30 15:55:31 +0700

    delete product

--- src/routes/product_route.js
+++ src/routes/product_route.js
@@ -7,6 +7,7 @@ router.get('/create', productController.create)
 router.get('/list', productController.list)
 router.get('/:_id/edit', productController.edit)
 router.put('/:_id/update', productController.update)
+router.delete('/:_id/delete', productController.destroy)
 router.post('/store', productController.store)
 router.get('/:id', productController.detail)
 
